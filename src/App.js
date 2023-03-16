e0fe527e276a8ce04f232fe3b4324ce688057c4b
ibuki <bigfrog0102@gmail.com>
2023-03-16 21:48:04 +0800

    initialized test project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import styles from './test.module.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={styles.Title}>Test Title</h1>
+    </div>
+  )
+}
+
+export default App

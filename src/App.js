88f7765a24810b639ece7239e721dc3547c834d1
chenchiahsiu <hebe12311@gmail.com>
2023-03-19 09:32:48 +0800

    刪掉一些沒用到的檔案及程式碼及新增元件資料夾與 Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css'
-import styles from './test.module.scss'
+import React from "react";
+
+import Header from "./components/Header/Header";
+
+import styles from "./test.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className={styles.Title}>Test Title</h1>
+    <div>
+      <Header />
+      <div className="App">
+        <h1 className={styles.Title}>Test Title</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "../Header/Header.module.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <div className={styles.Hi}>Hi</div>
+    </header>
+  );
+};
+
+export default Header;

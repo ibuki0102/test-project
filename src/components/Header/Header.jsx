c108f31b95995202d497f46995131e0bcebb1162
chenchiahsiu <hebe12311@gmail.com>
2023-03-19 14:48:11 +0800

    feat: add 1 line

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import styles from "../Header/Header.module.scss";
 
 const Header = () => {
-  return <div className={styles.Hi}>Hi</div>;
+  return (
+    <div className={styles.Hi}>
+      Hi
+      <div>hi</div>
+    </div>
+  );
 };
 
 export default Header;

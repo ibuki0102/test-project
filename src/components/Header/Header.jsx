ea8d71bbbc8b8ba326a3655566b92dd8b770fe30
ibuki <bigfrog0102@gmail.com>
2023-03-19 15:07:20 +0800

    feat: add one line after hi

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
-import styles from "../Header/Header.module.scss";
+import styles from '../Header/Header.module.scss'
 
 const Header = () => {
   return (
     <div className={styles.Hi}>
       Hi
       <div>hi</div>
+      <div>test</div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header

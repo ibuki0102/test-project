67269f2fe0106ad274700327cce3c101d91a1595
chenchiahsiu <hebe12311@gmail.com>
2023-03-19 13:44:21 +0800

    feat: add some files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React from "react";
-
-import Header from "./components/Header/Header";
-
-import styles from "./test.module.scss";
+import './App.css'
+import styles from './test.module.scss'
 
 function App() {
   return (
-    <div>
-      <Header />
-      <div className="App">
-        <h1 className={styles.Title}>Test Title</h1>
-      </div>
+    <div className="App">
+      <h1 className={styles.Title}>Test Title</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
